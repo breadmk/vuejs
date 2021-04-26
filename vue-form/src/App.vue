4a41b790b2dca0bde68a1631de657db549242449
breadmk <minvita9999@naver.com>
2021-04-26 15:43:13 +0900

    vue-form

---
+++ vue-form/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+      <div>
+        <label for="userName">id:</label>
+        <input type="text" id="username" v-model="username">
+      </div>
+      <div>
+        <label for="password">pw:</label>
+        <input type="password" id="password" v-model="password">
+      </div>
+      <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function(){
+    return {
+      username : "",
+      password : "",
+    }
+  },
+  methods: {
+    submitForm : function(){
+      // event.preventDefault();
+      console.log(this.username,this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        userName : this.username,
+        password : this.password
+      }
+      axios.post(url,data)
+        .then(function(response){
+          console.log(response);
+        })
+        .catch(function(error){
+          console.log(error);
+        })
+    }
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
